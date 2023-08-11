e65c2d5b80edb4ce411b4e23801605050bc77ce1
Ogie Galicia <o.galicia.cpa@gmail.com>
2023-08-11 11:24:11 +0800

    Feat: initial commits

---
+++ AutoSub/AutoSub.cs
@@ -0,0 +1,65 @@
+﻿namespace AutoSub;
+
+public class AutoSub
+{
+    private readonly Dictionary<Type, object?> _parameters = new();
+    private readonly Dictionary<string, (Type type, object? proxy)> _namedParameters = new();
+
+    public AutoSub Setup<T>(Action<ISetup<T>> func) where T : class
+    {
+        func.Invoke(new Setup<T>(_parameters));
+        return this;
+    }
+
+    public AutoSub Setup<T>(T value) where T : class
+    {
+        if (_parameters.ContainsKey(typeof(T)))
+            _parameters[typeof(T)] = value;
+        else
+            _parameters.Add(typeof(T), value);
+
+        return this;
+    }
+    
+    public AutoSub Setup<T>(string parameterName, T? value) where T : struct
+    {
+        _namedParameters.Add(parameterName, (typeof(T), value));
+        return this;
+    }
+
+    public AutoSub Setup<T>(string parameterName, T value) where T : class
+    {
+        _namedParameters.Add(parameterName, (typeof(T), value));
+        return this;
+    }
+
+    public T Create<T>()
+    {
+        var constructor = typeof(T).GetConstructors().First();
+        var parameters = constructor.GetParameters();
+        var parameterValues = new object[parameters.Length];
+        for (var i = 0; i < parameters.Length; i++)
+        {
+            var parameter = parameters[i];
+            if (_parameters.TryGetValue(parameter.ParameterType, out var value))
+            {
+                if (value is not null)
+                    parameterValues[i] = value;
+            }
+            else if (_namedParameters.TryGetValue(parameter.Name, out var namedValue))
+            {
+                parameterValues[i] = namedValue.proxy;
+            }
+            else if (parameter.ParameterType == typeof(string))
+            {
+                parameterValues[i] = string.Empty;
+            }
+            else
+            {
+                parameterValues[i] = Activator.CreateInstance(parameter.ParameterType)!;
+            }
+        }
+
+        return (T) constructor.Invoke(parameterValues);
+    }
+}
---
+++ AutoSub/Setup.cs
@@ -0,0 +1,35 @@
+﻿using NSubstitute;
+using NSubstitute.Core;
+
+namespace AutoSub;
+
+public interface ISetup<out T> where T : class
+{
+    ISetup<T> For(Func<T, ConfiguredCall> expression);
+}
+
+internal sealed class Setup<T> : ISetup<T> where T : class
+{
+    private readonly T _value;
+
+    public ISetup<T> For(Func<T, ConfiguredCall> expression)
+    {
+        expression.Invoke(_value);
+        return this;
+    }
+
+    public Setup(IDictionary<Type, object?> parameters)
+    {
+        var value = Substitute.For<T>();
+
+        var exist = parameters.TryGetValue(typeof(T), out var val);
+        if (exist) value = (T?) val;
+
+        if (parameters.ContainsKey(typeof(T)))
+            parameters[typeof(T)] = value;
+        else
+            parameters.Add(typeof(T), value);
+
+        _value = value!;
+    }
+}
