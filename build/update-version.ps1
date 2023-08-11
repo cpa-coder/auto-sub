b1eb5760788a60ab2d9d876b354fff4b3ee40733
Ogie Galicia <o.galicia.cpa@gmail.com>
2023-08-11 23:40:24 +0800

    Feat: add github action

---
+++ build/update-version.ps1
@@ -0,0 +1,13 @@
+﻿param($build_number, $project_path)
+
+Write-Host "Versioning started"
+"Build number " + ${build_number}
+$csprojfilename = ${project_path}
+"Project file to update " + $csprojfilename
+[xml]$csprojcontents = Get-Content -Path $csprojfilename;
+"Current version number is " + $csprojcontents.Project.PropertyGroup[1].Version
+$oldversionNumber = $csprojcontents.Project.PropertyGroup.Version
+$csprojcontents.Project.PropertyGroup[1].Version = ${build_number}
+$csprojcontents.Save($csprojfilename)
+"Version number has been udated from " + $oldversionNumber + " to " + ${build_number}
+Write-Host "Finished"
